9822e40636956cb54bae648b0fd6a1062ec7065d
Mervus <iolemcr2@gmail.com>
2024-08-17 00:12:30 +0200

    Actor and stage implemented

--- core/src/main/java/com/mervus/Main.java
+++ core/src/main/java/com/mervus/Main.java
@@ -10,6 +10,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
@@ -42,6 +43,7 @@ public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
     private Rectangle bucket;
     private Texture bucketImage;
+    Stage stage;
     public final static float WIDTH = 500;
     public final static float HEIGHT = 16 * WIDTH / 9;
 
@@ -54,13 +56,21 @@ public class Main extends ApplicationAdapter {
     @Override
     public void create() {
         camera = new OrthographicCamera(50, 50);
-        viewport = new FitViewport(640, 480, camera);
+        viewport = new FitViewport(1440, 1080, camera);
         batch = new SpriteBatch();
+        stage = new Stage(viewport, batch);
+
+        Gdx.input.setInputProcessor(stage);
+
         mapData = new HashMap<Vector2, TileData>();
         tiles = MapGenerator.generateTiles(mapData);
+        mapData.forEach((i, o) -> {
+            stage.addActor(o.tile);
+        });
         entities = new ArrayList<Entity>();
         Elf elf = new Elf(new Vector2(0,0));
         entities.add(elf);
+        stage.addActor(elf);
         mapData.get(elf.position).entity = elf;
         //entities.add(new Dwarf(new Vector2(115,0)));
     }
@@ -68,29 +78,18 @@ public class Main extends ApplicationAdapter {
     @Override
     public void render() {
         float delta = Gdx.graphics.getDeltaTime();
-
+        stage.act(delta);
+        stage.draw();
         camera.update();
-        batch.begin();
-        for (int i = 0; i < tiles.length; i++) {
-            tiles[i].render(batch);
-        }
-        batch.end();
-
-        batch.begin();
-        for (int i = 0; i < entities.size(); i++)
-        {
-            entities.get(i).render(batch);
-        }
-        batch.end();
     }
 
     @Override
     public void resize(int width, int height) {
-        viewport.update(width, height);
+        stage.getViewport().update(width, height, true);
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
+        stage.dispose();
     }
 }
